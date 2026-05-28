fdb6af23a7a08648aee854fa517627efaa97a5c9
Masatoshi Yamada <yamadaxms.gmail.com>
2026-05-28 15:15:26 +0900

    v055

--- edit/config.js
+++ edit/config.js
@@ -6,7 +6,7 @@
     adminApiPath: "/api/admin/tables",
     auth: {
       region: "ap-northeast-1",
-      cognitoDomain: "https://aynu-edit-auth.auth.ap-northeast-1.amazoncognito.com",
+      cognitoDomain: "https://ap-northeast-1trcwtrr0a.auth.ap-northeast-1.amazoncognito.com",
       clientId: "7sp6htb7pr10pbltfsv4giu6ag",
       tokenUse: "accessToken",
       redirectUri: currentPageUrl,
